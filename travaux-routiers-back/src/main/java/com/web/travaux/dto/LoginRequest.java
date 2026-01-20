3ac4892880f0eaea783454dd6cd8ac0a50afb2f9
Randy <randyrobinson3227@gmail.com>
2026-01-20 12:18:04 +0300

    f

--- travaux-routiers-back/src/main/java/com/web/travaux/dto/LoginRequest.java
+++ travaux-routiers-back/src/main/java/com/web/travaux/dto/LoginRequest.java
@@ -2,13 +2,9 @@ package com.web.travaux.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;
 }
-
-
