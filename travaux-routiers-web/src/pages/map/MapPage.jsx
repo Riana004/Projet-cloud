1b6b77e1bf2e75871043f973a955fd504e3820dc
Randy <randyrobinson3227@gmail.com>
2026-01-20 09:02:54 +0300

    f

--- travaux-routiers-web/src/pages/map/MapPage.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/map/MapPage.jsx
-import { useState } from "react";
-import { reports as mockReports } from "../../data/reports.mock";
-import MapView from "../../components/Map/MapView";
-
-export default function MapPage() {
-  const [reports, setReports] = useState(mockReports);
-  return <MapView reports={reports} />;
-}
