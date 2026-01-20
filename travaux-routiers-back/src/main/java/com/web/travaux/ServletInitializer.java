73626a271cff3517bc92139dc02222e254a7446f
Riana <rmitariana@gmail.com>
2026-01-20 20:51:35 +0300

    package

--- travaux-routiers-back/src/main/java/com/web/travaux/ServletInitializer.java
+++ travaux-routiers-back/src/main/java/com/web/travaux/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.authentification.authentification;
+package com.web.travaux;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
