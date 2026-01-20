79df2fc36d09b804d73a5403f776360da5229dac
Randy <randyrobinson3227@gmail.com>
2026-01-20 12:18:04 +0300

    f

--- travaux-routiers-back/src/main/java/com/web/travaux/controller/AuthController.java
+++ travaux-routiers-back/src/main/java/com/web/travaux/controller/AuthController.java
@@ -27,7 +27,6 @@ public class AuthController {
                                 user.getId(),
                                 user.getEmail(),
                                 String.valueOf(user.getRole().getId()) // on renvoie l'id du rôle
-
                         );
                         return ResponseEntity.ok(response);
                     } else {
