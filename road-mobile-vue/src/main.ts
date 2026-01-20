74efedefde782def095d32cded114331afabdf45
eliaTolojanahary <tolojanaharyelia845@gmail.com>
2026-01-20 16:09:17 +0100

    go mobile

---
+++ road-mobile-vue/src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import { IonicVue } from '@ionic/vue'
+
+import '@ionic/vue/css/core.css'
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
+import './theme/variables.css'
+
+createApp(App)
+  .use(IonicVue)
+  .use(router)
+  .mount('#app')
