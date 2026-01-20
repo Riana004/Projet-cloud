ae1e91defb4e2fb8805ad59fccb5d49391a8c52a
Riana <rmitariana@gmail.com>
2026-01-20 20:51:35 +0300

    package

--- travaux-routiers-back/src/main/java/com/web/travaux/AuthentificationApplication.java
+++ travaux-routiers-back/src/main/java/com/web/travaux/AuthentificationApplication.java
@@ -1,4 +1,4 @@
-package com.authentification.authentification;
+package com.web.travaux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
